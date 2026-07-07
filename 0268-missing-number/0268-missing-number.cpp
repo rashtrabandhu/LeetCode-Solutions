1e6c8fb80c8a5f218056e5a942889bc8dc827094
Rashtra Bandhu <rashtrabandhu145@gmail.com>
2026-07-07 20:52:26 +0530

    Time: 0 ms (100%), Space: 21.7 MB (70.71%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n=nums.size();
+        int sum=0;
+
+        int Tsum=n*(n+1)/2;
+
+
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+        }
+
+        return Tsum-sum;
+
+        
+    }
+};
